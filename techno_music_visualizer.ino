a0277d587975b278c7806f7a36b758444f0e1814
Tane Tachyon <tachyon@tachyonlabs.com>
2018-03-08 14:14:34 -0800

    Added delay speed configurability

--- techno_music_visualizer.ino
+++ techno_music_visualizer.ino
@@ -6,6 +6,19 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 
+// I'm seeing best results using band 4 to isolate the drumbeats
+#define DRUM_BAND 4
+
+// Depending on the music you're playing and the volume you have it set to, you may need to tweak either 
+// the volume or this level to respond to all the drumbeats but not to other elements of the music
+#define DRUM_VOLUME_THRESHOLD 170
+
+// Leave this at 1 for a good basic fade, or decrease it to 0 for a quicker fade, or increase it to 2, 3, 
+// etc. for a more drawn-out fade
+#define FADE_DELAY 1
+
+int fadeCountdown = FADE_DELAY;
+
 CRGB leds[NUM_LEDS];
 
 //Declare Spectrum Shield pin connections
@@ -13,13 +26,6 @@ CRGB leds[NUM_LEDS];
 #define RESET 5
 #define DC_One A0
 #define DC_Two A1 
-
-// I'm seeing best results using band 4 to isolate the drumbeats
-#define DRUM_BAND 4
-
-// Depending on the music you're playing and the volume you have it set to, you may need to tweak either the 
-// volume or this level to respond to all the drumbeats but not to other elements of the music
-#define DRUM_VOLUME_THRESHOLD 170
 
 //Define spectrum variables
 int Frequencies_One[7];
@@ -63,10 +69,15 @@ void turnOffAllLeds() {
 }
 
 void listenAndFade() {
-  // we're fading all the time, but only going to 100% brightness when there's a beat
+  // We're fading all the time, but only going to 100% brightness when there's a beat
   Read_Frequencies();
-  fadeToBlackBy(leds, NUM_LEDS, 1);
-  FastLED.show();
+  if (fadeCountdown == 0) {
+    fadeToBlackBy(leds, NUM_LEDS, 1);
+    FastLED.show();
+    fadeCountdown = FADE_DELAY;
+  } else {
+    fadeCountdown--;
+  }
 }
 
 /*************Pull frquencies from Spectrum Shield****************/
@@ -76,9 +87,9 @@ void Read_Frequencies() {
     Frequencies_One[freq_amp] = analogRead(DC_One);
     Frequencies_Two[freq_amp] = analogRead(DC_Two); 
     if (freq_amp == DRUM_BAND && (Frequencies_One[DRUM_BAND] > DRUM_VOLUME_THRESHOLD || Frequencies_Two[DRUM_BAND] > DRUM_VOLUME_THRESHOLD)) {
-      // we're fading all the time, but only going to 100% brightness when there's a beat
-      fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
-      // might as well have some color variation
+      // We're fading all the time, but only going to 100% brightness when there's a beat
+      fill_solid(leds, NUM_LEDS, CHSV(hue, 255, BRIGHTNESS));
+      // Might as well have some color variation
       hue = (hue + 1) % 256;
     }
     digitalWrite(STROBE, HIGH);
